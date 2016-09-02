2589ad74d4ef58d7b550901a9b60d9994e87fff0
Sereda-Fazen <fazen7@mail.ru>
2016-09-02 13:32:14 +0300

    test 2

--- tests/acceptance/02_CheckMenuCest.php
+++ tests/acceptance/02_CheckMenuCest.php
@@ -7,13 +7,25 @@ use \Step\Acceptance;
 class CheckMenuCest
 {
 
-    function checkMenu( \Page\CheckMenu $menu,\Step\Acceptance\Steps $I)
+    function mainMenuSearch( \Page\CheckMenu $menu,\Step\Acceptance\Steps $I)
     {
-        $I->login('yvikajvd@grandmasmail.com','123456789');
+        $I->login('yvikajvd@grandmasmail.com', '123456789');
         $menu->mainMenuSearch();
+    }
+    function mainMenuGSMMaster( \Page\CheckMenu $menu,\Step\Acceptance\Steps $I)
+    {
         $menu->mainMenuGSMMaster();
+    }
+    function mainMenuManagerUsers( \Page\CheckMenu $menu,\Step\Acceptance\Steps $I)
+    {
         $menu->mainMenuManagerUsers();
+    }
+    function mainMenuContacts( \Page\CheckMenu $menu,\Step\Acceptance\Steps $I)
+    {
         $menu->mainMenuContacts();
+    }
+    function checkInvalidUrl( \Page\CheckMenu $menu,\Step\Acceptance\Steps $I)
+    {
         $menu->invalidUrl('invalid.html');
     }
 
